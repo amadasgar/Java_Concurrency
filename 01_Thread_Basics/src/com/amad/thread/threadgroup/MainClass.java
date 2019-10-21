9106e8dff575e92441b9ad0db3d9025175662923
amadasgar <amad.151214@gmail.com>
2019-10-21 23:32:05 +0530

    ThreadGroup

---
+++ 01_Thread_Basics/src/com/amad/thread/threadgroup/MainClass.java
@@ -0,0 +1,22 @@
+package com.amad.thread.threadgroup;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		int numberOfThreads = 2 * Runtime.getRuntime().availableProcessors();
+		MyThreadGroup threadGroup = new MyThreadGroup("MyThreadGroup");
+		Task task = new Task();
+		for (int i = 0; i < numberOfThreads; i++) {
+			Thread t = new Thread(threadGroup, task);
+			t.start();
+		}
+		System.out.printf("Number of Threads: %d\n", threadGroup.activeCount());
+		System.out.printf("Information about the Thread Group\n");
+		threadGroup.list();
+		Thread[] threads = new Thread[threadGroup.activeCount()];
+		threadGroup.enumerate(threads);
+		for (int i = 0; i < threadGroup.activeCount(); i++) {
+			System.out.printf("Thread %s: %s\n", threads[i].getName(), threads[i].getState());
+		}
+	}
+}
